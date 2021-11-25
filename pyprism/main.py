d3b1be7e51412b9ea1f6c1616ed1ae49ecc5a36c
Ryosuke Kojima <kojima.ryosuke.8e@kyoto-u.ac.jp>
2021-11-25 18:23:09 +0000

    simplify outputs

--- pyprism/main.py
+++ pyprism/main.py
@@ -5,7 +5,7 @@ import datetime as dt
 import argparse
 import typing as t
 
-def run(code, args=[]):
+def run_(code, args=[]):
     prism_wd_path = './.prism_code/'
     now = dt.datetime.now()
     os.makedirs(prism_wd_path,exist_ok=True)
@@ -14,12 +14,21 @@ def run(code, args=[]):
         fp.write(code)
     return run_file(filename,args)
 
-def run_file(filename, args=[]):
+def run(code, args=[]):
+    r=run_(code,args)
+    return r.stdout.decode("utf8")
+
+
+def run_file_(filename, args=[]):
     path=os.path.dirname(__file__)
     cmd=path+"/bin/upprism"
     cmds=[cmd, filename]+args
     out=subprocess.run(cmds,timeout=None,stdout = subprocess.PIPE, stderr = subprocess.PIPE)
     return out
+
+def run_file(filename, args=[]):
+    r=run_file_(filename,args)
+    return r.stdout.decode("utf8")
 
 def main(argv: t.Optional[t.List[str]] = None) -> None:
     parser = argparse.ArgumentParser(
